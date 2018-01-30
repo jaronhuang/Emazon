ad1dcc1c76001b9b27aecdc2ac3b3d5aa0642a2b
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-30 12:46:04 -0500

    final for 1/30/18

--- Emazon/src/Runner.java
+++ Emazon/src/Runner.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -22,11 +24,21 @@ public class Runner extends Application {
 		primaryStage.setTitle("Emazon");
 		
 		Button shoppingCartButton = new Button("Cart");
+		shoppingCartButton.setTranslateY(30);
+		
 		Button carsButton = new Button("Cars");
+		carsButton.setTranslateX(-200);
+		
 		Button audioBookButton = new Button("Audio Books");
+		audioBookButton.setTranslateX(-100);
+		
 		Button technology = new Button("Technology");
+		
 		Button clothing = new Button("Clothing");
+		clothing.setTranslateX(100);
+		
 		Button shoes = new Button("Shoes");
+		shoes.setTranslateX(200);
 		
 		shoppingCartButton.setOnAction(new EventHandler<ActionEvent>() {
 		    @Override
@@ -36,28 +48,24 @@ public class Runner extends Application {
 		});
 		
 		
-		GridPane gridPane = new GridPane();
-	    gridPane.setAlignment(Pos.TOP_CENTER);
-	    gridPane.setHgap(10);
-	    gridPane.setVgap(10);
-	    gridPane.setPadding(new Insets(10));
-	    gridPane.setGridLinesVisible(true);
-	    
-	    
-	    gridPane.add(shoppingCartButton, 3, 2);
-	    gridPane.add(carsButton, 3, 0);
-	    gridPane.add(audioBookButton, 1, 0);
-	    gridPane.add(technology, 2, 0);
-	    gridPane.add(shoes, 4, 0);
-	    gridPane.add(clothing, 5, 0);
-	    
-	    VBox root = new VBox();
+		StackPane stack = new StackPane();
+		stack.getChildren().add(shoppingCartButton);
+		stack.getChildren().add(carsButton);
+		stack.getChildren().add(audioBookButton);
+		stack.getChildren().add(technology);
+		stack.getChildren().add(clothing);
+		stack.getChildren().add(shoes);
 		
-	   
+	    VBox root = new VBox();
+		root.setStyle("-fx-border-color: #0000cc; -fx-border-width: 5px;");
+		
+		VBox homePage = new VBox();
+		homePage.setStyle("-fx-border-color: #00cc00; -fx-border-width: 5px;");
+		homePage.setPrefHeight(300);
+		homePage.setPrefWidth(200);
+		
 	    root.setSpacing(50);
-	    root.getChildren().add(gridPane);
-	    GridPane.setHalignment(shoppingCartButton, HPos.CENTER);
-	    GridPane.setValignment(shoppingCartButton, VPos.BASELINE);
+	    root.getChildren().add(stack);
         Scene scene = new Scene(root, 450, 700);
         primaryStage.setTitle("Emazon");
 	    primaryStage.setScene(scene);
