a0549494c411528093d918d59ec5a1f05905285d
jbernard3853 <32273189+jbernard3853@users.noreply.github.com>
2018-02-07 12:28:49 -0500

    Update CSVUtilities.java

--- Emazon/src/CSVUtilities.java
+++ Emazon/src/CSVUtilities.java
@@ -4,7 +4,10 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -46,7 +49,7 @@ public class CSVUtilities
 		return Arrays.asList(CSVData.get(0).split(","));
 	}
 	
-	public List<String> getDataString(int column)
+	public ArrayList<String> getDataString(int column)
 	{
 		ArrayList<String> data = new ArrayList<String>();
 		for (int i = 1; i < CSVData.size(); i++)
@@ -57,7 +60,7 @@ public class CSVUtilities
 		return data;
 	}
 	
-	public List<Integer> getDataInteger(int column)
+	public ArrayList<Integer> getDataInteger(int column)
 	{
 		ArrayList<Integer> data = new ArrayList<Integer>();
 		for (int i = 1; i < CSVData.size(); i++)
@@ -68,7 +71,7 @@ public class CSVUtilities
 		return data;
 	}
 	
-	public List<Double> getDataDouble(int column)
+	public ArrayList<Double> getDataDouble(int column)
 	{
 		ArrayList<Double> data = new ArrayList<Double>();
 		for (int i = 1; i < CSVData.size(); i++)
@@ -78,5 +81,29 @@ public class CSVUtilities
 		}
 		return data;
 	}
-	
+		 
+	static File file = new File("cart.csv");
+	public static void writeCSV(String name, int quantity, double price, String image)
+	{
+		CSVUtilities csv = null;
+		try 
+		{
+			csv = new CSVUtilities(file);
+		} 
+		catch (IOException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		int line = 1;
+		csv.getCSVData().add(line, name + "," + quantity + "," + (price * quantity) + "," + image);
+		try 
+		{
+			Files.write(Paths.get("cart.csv"), csv.getCSVData());
+		} 
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
